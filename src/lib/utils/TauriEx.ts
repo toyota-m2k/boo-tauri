a5d8d2f88d30c0f834c7e9c4a541022786fcd37c
toyota-m2k <toyota.mitsuki@gmail.com>
2024-05-03 10:40:45 +0900

    check file saving error

--- src/lib/utils/TauriEx.ts
+++ src/lib/utils/TauriEx.ts
@@ -1,8 +1,11 @@
-import { getCurrent, appWindow } from '@tauri-apps/api/window'
-import { os } from '@tauri-apps/api'
+import {getCurrent} from '@tauri-apps/api/window'
+import {os} from '@tauri-apps/api'
 import {logger} from "../model/DebugLog";
+import {BaseDirectory, createDir, type FsOptions, readTextFile, writeTextFile} from "@tauri-apps/api/fs";
 
 export type OSPlatForm = "L" | "M" | "W" | "U" | "LB" | "MB" | "WB" | "UB"
+
+export type FSType = "uav" | "appdata" | "app" | "default"
 
 class TauriEx {
 
@@ -42,17 +45,23 @@ class TauriEx {
     }
   }
 
+  os: OSPlatForm|undefined = undefined
   async getOS(): Promise<OSPlatForm> {
+    if(this.os) {return this.os }
     try {
       switch (await os.type()) {
         case "Linux":
-          return "L"
+          this.os = "L"
+          break
         case "Darwin":
-          return "M"
+          this.os = "M"
+          break
         case "Windows_NT":
-          return "W"
+          this.os = "W"
+          break
         default:
-          return "U"
+          this.os = "U"
+          break
       }
     } catch (e) {
       logger.info("it may not be tauri")
@@ -62,15 +71,16 @@ class TauriEx {
         windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE']
 
       if (macosPlatforms.indexOf(platform) !== -1) {
-        return 'MB'
+        this.os = 'MB'
       } else if (windowsPlatforms.indexOf(platform) !== -1) {
-        return 'WB'
+        this.os = 'WB'
       } else if (!os && /Linux/.test(platform)) {
-        return 'LB'
+        this.os = 'LB'
       } else {
-        return 'UB'
+        this.os = 'UB'
       }
     }
+    return this.os
   }
 
   async setFocusListener(listener: (focused: boolean) => void): Promise<()=>void> {
@@ -93,6 +103,136 @@ class TauriEx {
     } catch(error) {
       logger.error('Failed to set window close listener')
       return ()=>{}
+    }
+  }
+
+
+  fsType: FSType|undefined = undefined
+
+  private async checkFsAndRead(filename:string, ops?: FsOptions): Promise<{result:boolean, data:string}> {
+    const result= {result: false, data: ""}
+    try {
+      result.data = await readTextFile(filename, ops)
+      result.result = true
+    } catch(e) {
+      logger.error('read error :' + e)
+      try {
+        await writeTextFile(filename, "{}", ops)
+        result.data = "{}"
+        result.result = true
+      } catch (e) {
+        logger.error('write error: ' + e)
+      }
+    }
+    return result
+  }
+
+
+  async readJson(filename:string): Promise<string|undefined> {
+    let data: string
+    if (!this.fsType) {
+      if ((await this.getOS()).length !== 1) {
+        // browser mode
+        this.fsType = "uav"
+        return undefined
+      } else {
+        let r = await this.checkFsAndRead(filename, {dir: BaseDirectory.AppData})
+        if (r.result) {
+          logger.info('fs:appdata available')
+          this.fsType = "appdata"
+          return r.data
+        }
+        r = await this.checkFsAndRead(filename, {dir: BaseDirectory.App})
+        if (r.result) {
+          logger.info('fs:app available')
+          this.fsType = "app"
+          return r.data
+        }
+        r = await this.checkFsAndRead(filename)
+        if (r.result) {
+          logger.info('fs:default available')
+          this.fsType = "default"
+          return r.data
+        }
+        this.fsType = "uav"
+        return undefined
+      }
+    } else {
+      try {
+        logger.info('fsType: ' + this.fsType)
+        switch (this.fsType) {
+          case "appdata":
+            data = await readTextFile(filename, {dir: BaseDirectory.AppData})
+            break
+          case "app":
+            data = await readTextFile(filename, {dir: BaseDirectory.App})
+            break
+          case "default":
+            data = await readTextFile(filename)
+            break
+          default:
+            return undefined
+        }
+      } catch (e) {
+        logger.error('Failed to read file: ' + e)
+        return undefined
+      }
+    }
+  }
+
+  async writeJson(filename:string, data:string): Promise<boolean> {
+    if (true/*!this.fsType*/) {
+      if ((await this.getOS()).length !== 1) {
+        // browser mode
+        this.fsType = "uav"
+        return false
+      } else {
+        try {
+          await writeTextFile(filename, data, {dir: BaseDirectory.AppData})
+          this.fsType = "appdata"
+          return true
+        } catch (e) {
+          logger.error('Failed (appdata): ' + e)
+        }
+
+        try {
+          await writeTextFile(filename, data, {dir: BaseDirectory.App})
+          this.fsType = "app"
+          return true
+        } catch (e) {
+          logger.error('Failed (app): ' + e)
+        }
+
+        try {
+          await writeTextFile(filename, data)
+          this.fsType = "default"
+          return true
+        } catch (e) {
+          logger.error('Failed (default): ' + e)
+        }
+        this.fsType = "uav"
+        return false
+      }
+    } else {
+      try {
+        logger.info('fsType: ' + this.fsType)
+        switch (this.fsType) {
+          case "appdata":
+            await writeTextFile(filename, data, {dir: BaseDirectory.AppData})
+            return true
+          case "app":
+            await writeTextFile(filename, data, {dir: BaseDirectory.App})
+            return true
+          case "default":
+            await writeTextFile(filename, data)
+            return true
+          default:
+            return false
+        }
+      } catch (e) {
+        logger.error('Failed to write file: ' + e)
+        return false
+      }
     }
   }
 }
